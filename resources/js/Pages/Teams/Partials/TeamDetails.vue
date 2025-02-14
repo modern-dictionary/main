387df5256ba70629bab9bce61f45ed86cfbe1fad
KasraMehr <kasramehrali@gmail.com>
2025-02-14 07:57:43 -0500

    fixed UI

--- resources/js/Pages/Teams/Partials/TeamDetails.vue
+++ resources/js/Pages/Teams/Partials/TeamDetails.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-        <div class="bg-gray-900 p-6 rounded-lg w-full max-w-2xl">
-            <button @click="$emit('close')"
-                class="mb-4 px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700">
-                {{ $t('back') }}
-            </button>
+    <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+      @click="$emit('close')">
+        <div class="bg-gray-200 dark:bg-gray-900 p-6 rounded-lg w-full max-w-2xl">
 
-            <h2 class="text-2xl font-bold">{{ team.name }}</h2>
+            <h2 class="text-2xl font-bold text-black dark:text-white">{{ team.name }}</h2>
             <p class="text-gray-400">{{ $t('team_owner') }}: {{ team.owner.name }}</p>
             <p class="text-gray-400">{{ $t('member_count') }}: {{ 1 + team.users_count }}</p>
             <p class="text-gray-400">{{ $t('word_count') }}: {{ team.words_count }}</p>
 
             <!-- لیست اعضا -->
-            <h3 class="mt-4 text-lg font-semibold">{{ $t('users') }}</h3>
-            <ul class="space-y-2 mt-2">
-                <li v-for="user in team.users" :key="user.id" class="p-2 bg-gray-700 rounded">
-                    {{ user.name }} ({{ user.email }})
-                </li>
-            </ul>
+            <h3 class="mt-4 text-lg font-semibold text-black dark:text-white">{{ $t('members') }}</h3>
+            <div class="space-y-2 mt-2">
+                <div v-for="user in team.users" :key="user.id" class="grid grid-cols-2 p-2 bg-gray-400 dark:bg-gray-700 rounded">
+                  <div class="text-black dark:text-white">
+                    {{ user.name }}
+                  </div>
+                  <div class="text-black dark:text-white">
+                    {{ user.email }}
+                  </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
